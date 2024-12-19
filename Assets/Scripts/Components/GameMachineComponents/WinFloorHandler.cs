356576835125d512b1b5661cc9bce40c849f319c
Ceclik <yura11122345@gmail.com>
2024-12-19 13:28:03 +0300

    Added textures lights and collected toys counter

--- Assets/Scripts/Components/GameMachineComponents/WinFloorHandler.cs
+++ Assets/Scripts/Components/GameMachineComponents/WinFloorHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Components.CharacterComponents;
+using TMPro;
 using UnityEngine;
 
 namespace Components.GameMachineComponents
@@ -7,14 +8,33 @@ namespace Components.GameMachineComponents
     public class WinFloorHandler : MonoBehaviour
     {
         [SerializeField] private Transform winPlace;
-        
+        [SerializeField] private TextMeshProUGUI toysText;
+
+        private int _counter;
+        private GameObject _previousToy;
+
+        public int ToysCount
+        {
+            get => _counter;
+            set
+            {
+                _counter = value;
+                UpdateToysText();
+            }
+        }
         private Transform _claw;
         private CoinRegisterHandler _coinRegister;
         private ClawMovementHandler _movementHandler;
         private CoinsHandler _coins;
 
+        private void UpdateToysText()
+        {
+            toysText.text = ToysCount.ToString();
+        }
+
         private void Start()
         {
+            ToysCount = _counter;
             _movementHandler = GameObject.FindGameObjectWithTag("Claw").GetComponent<ClawMovementHandler>();
             _coinRegister = GameObject.FindGameObjectWithTag("CoinChecker").GetComponent<CoinRegisterHandler>();
             _claw = GameObject.FindGameObjectWithTag("Claw").transform;
@@ -25,12 +45,19 @@ namespace Components.GameMachineComponents
         {
             if (other.gameObject.CompareTag("Toy"))
             {
+                if (_previousToy != null)
+                {
+                    Destroy(_previousToy);
+                    _previousToy = other.gameObject;
+                }
+
+                ToysCount++;
                 _claw.GetComponent<Collider>().isTrigger = false;
                 other.transform.position = winPlace.transform.position;
                 _coinRegister.IsCoinThrown = false;
                 _movementHandler.HasFallen = false;
                 _coins.CoinsAmount += 2;
-                other.rigidbody.AddTorque(1, 0, 0);
+                other.rigidbody.AddTorque(5, 0, 0);
             }
         }
     }
